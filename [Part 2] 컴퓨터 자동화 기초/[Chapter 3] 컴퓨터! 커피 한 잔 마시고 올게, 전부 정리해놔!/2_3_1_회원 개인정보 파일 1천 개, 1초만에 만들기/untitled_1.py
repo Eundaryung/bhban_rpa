c8566a82247583ce4965b166ca1052272a10fd42
Eundaryung <safelg83@gmail.com>
2022-05-18 15:15:47 +0900

    2022.5.18 연습

---
+++ [Part 2] 컴퓨터 자동화 기초/[Chapter 3] 컴퓨터! 커피 한 잔 마시고 올게, 전부 정리해놔!/2_3_1_회원 개인정보 파일 1천 개, 1초만에 만들기/untitled_1.py
@@ -0,0 +1,11 @@
+print("Start process.")
+import random
+import math
+
+for i in range(9):
+    number_age=str(random.randint(1,99))
+    print(number_age)
+    number_int_age=str(math.ceil(random.random()))
+    print(number_int_age)
+
+print("Process ends.")
